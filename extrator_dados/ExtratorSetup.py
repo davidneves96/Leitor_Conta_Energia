f10f8e3b54bc5b4166d1cc66b774bdd8849599b7
JORGE DAVID <david.neves96@hotmail.com>
2023-12-28 20:41:26 -0300

    ajuste na funcao

--- extrator_dados/ExtratorSetup.py
+++ extrator_dados/ExtratorSetup.py
@@ -1,9 +1,9 @@
 class ExtratorSetup:
-def __init__(self):
-    # Configurações específicas do seu ambiente
-    self.location_hdfs = "/caminho/no/hdfs"
-    self.nome_tabela_hive = "nome_da_sua_tabela"
-    # Adicione outras configurações conforme necessário
+    def __init__(self):
+        # Configurações específicas do seu ambiente
+        self.location_hdfs = "/caminho/no/hdfs"
+        self.nome_tabela_hive = "nome_da_sua_tabela"
+        # Adicione outras configurações conforme necessário
 
-    # Caminho do PDF
-    caminho_pdf = "caminho/do/seu/arquivo.pdf"
+        # Caminho do PDF
+        caminho_pdf = "caminho/do/seu/arquivo.pdf"
