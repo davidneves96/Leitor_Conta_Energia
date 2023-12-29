3c07db396699dae0465404dd2f2a17808980b91a
JORGE DAVID <david.neves96@hotmail.com>
2023-12-28 22:19:22 -0300

    removido itens no setup que nao serao usado

--- extrator_dados/ExtratorSetup.py
+++ extrator_dados/ExtratorSetup.py
@@ -2,8 +2,3 @@ class ExtratorSetup:
     def __init__(self):
         # Configurações específicas do seu ambiente
         self.location_hdfs = "hdfs://localhost:9000/bases/"
-        # self.nome_tabela_hive = "nome_da_sua_tabela"
-        # Adicione outras configurações conforme necessário
-
-        # Caminho do PDF
-        caminho_pdf = "caminho/do/seu/arquivo.pdf"
